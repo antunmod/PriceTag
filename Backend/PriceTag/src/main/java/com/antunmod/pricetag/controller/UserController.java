afb087ca5093c6fa2d75e6b5f99dcdf24ca07b84
antunmod <antun.mod@gmail.com>
2018-01-05 12:10:24 +0100

    Test User added to database

--- Backend/PriceTag/src/main/java/com/antunmod/pricetag/controller/UserController.java
+++ Backend/PriceTag/src/main/java/com/antunmod/pricetag/controller/UserController.java
@@ -1,10 +1,41 @@
 package com.antunmod.pricetag.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.sql.Date;
+import java.util.Calendar;
 
-@RestController
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.antunmod.pricetag.model.User;
+import com.antunmod.pricetag.repo.UserRepository;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
 
-	public 
+	@Autowired
+	private UserRepository userRepository;
+	
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String showUserList (Model model) {
+		String result = "<html>";
+        
+        for(User user : userRepository.findAll()){
+            result += "<div>" + user.toString() + "</div>";
+        }
+          
+        return result + "</html>";
+	}
+	
+	@RequestMapping("/save")
+	public String saveUser() {
+		userRepository.save(new User("newUser", "newpass", "example@pricetag.com", new Date(Calendar.getInstance().getTime().getTime()), 0, 1));
+		return "saved";
+	}
+	
+	
 	
 }
--- Backend/PriceTag/src/main/java/com/antunmod/pricetag/model/User.java
+++ Backend/PriceTag/src/main/java/com/antunmod/pricetag/model/User.java
@@ -1,9 +1,119 @@
 package com.antunmod.pricetag.model;
 
-import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="user")
-public class User {
+public class User implements Serializable{
+	
+	/**
+	 * auto generated serialVersionUID
+	 */
+	private static final long serialVersionUID = 8234325017646826418L;
+
+	@Id
+	@Column (name = "user_ID")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long userId;
+	
+	@Column (name = "user_name")
+	private String name;
+	
+	@Column (name = "user_password")
+	private String password;
+	
+	@Column (name = "user_mail")
+	private String email;
+	
+	@Column (name = "signup_date")
+	private Date signupDate;
+	
+	@Column (name = "points")
+	private int points;
+	
+	@Column (name = "user_type_ID")
+	private int userType;
+
+	
+	
+	public User() {
+		super();
+	}
+
+	public User(String name, String password, String email, Date signupDate, int points, int userType) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.email = email;
+		this.signupDate = signupDate;
+		this.points = points;
+		this.userType = userType;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getSignupDate() {
+		return signupDate;
+	}
+
+	public void setSignupDate(Date signupDate) {
+		this.signupDate = signupDate;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public int getUserType() {
+		return userType;
+	}
+
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+	
 	
 }
