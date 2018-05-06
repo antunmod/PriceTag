7d3f71de98449928208f6d3a1bd762c6cedd51da
antunmod <antun.mod@gmail.com>
2018-05-06 23:58:05 +0200

    Added services to reducing SelectFragment size

--- Frontend/PriceTag/app/src/main/java/antunmod/projects/pricetag/service/SelectService.java
+++ Frontend/PriceTag/app/src/main/java/antunmod/projects/pricetag/service/SelectService.java
@@ -1,8 +1,129 @@
 package antunmod.projects.pricetag.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import antunmod.projects.pricetag.RestServiceClient;
+import antunmod.projects.pricetag.view.fragment.SelectFragment;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 /**
- * Created by antun on 5/6/2018.
+ * A service for handling retrofit calls from SelectFragment.
  */
 
 public class SelectService {
+
+    private final String ERROR_STRING = "Došlo je do greške, pokušajte ponovo";
+
+    public void findStoreAddresses(String selectedStore) {
+        RestServiceClient restServiceClient = RestServiceClient.retrofit.create(RestServiceClient.class);
+        Call<List<String>> call = restServiceClient.getStoreLocations(selectedStore);
+        call.enqueue(new Callback<List<String>>() {
+            @Override
+            public void onResponse(Call<List<String>> call, Response<List<String>> response) {
+                ArrayList<String> storeAddresses = (ArrayList<String>) response.body();
+                if (storeAddresses != null) {
+                    SelectFragment.setStoreList(storeAddresses);
+                } else {
+                    SelectFragment.setErrorString(ERROR_STRING);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<String>> call, Throwable t) {
+                SelectFragment.setErrorString(ERROR_STRING);
+            }
+        });
+    }
+
+    public void findSectors(final String storeAddress) {
+
+        RestServiceClient restServiceClient = RestServiceClient.retrofit.create(RestServiceClient.class);
+        Call<List<String>> call = restServiceClient.getAllSectorNames();
+        call.enqueue(new Callback<List<String>>() {
+            @Override
+            public void onResponse(Call<List<String>> call, Response<List<String>> response) {
+                List<String> sectorList = response.body();
+
+                if (sectorList != null) {
+                    SelectFragment.setSectorList(sectorList);
+                } else {
+                    SelectFragment.setErrorString(ERROR_STRING);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<String>> call, Throwable t) {
+                SelectFragment.setErrorString(ERROR_STRING);
+            }
+        });
+    }
+
+    public void findStoreId(String storeAddress) {
+
+        RestServiceClient restServiceClient = RestServiceClient.retrofit.create(RestServiceClient.class);
+        Call<Byte> call = restServiceClient.getStoreIdForAddress(storeAddress);
+        call.enqueue(new Callback<Byte>() {
+            @Override
+            public void onResponse(Call<Byte> call, Response<Byte> response) {
+                Byte storeId = response.body();
+
+                if (storeId != null) {
+                    SelectFragment.setStoreId(storeId);
+                } else {
+                    SelectFragment.setErrorString(ERROR_STRING);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Byte> call, Throwable t) {
+                SelectFragment.setErrorString(ERROR_STRING);
+            }
+        });
+    }
+
+    public void findCategoriesForSectorName(String sectorName) {
+        RestServiceClient restServiceClient = RestServiceClient.retrofit.create(RestServiceClient.class);
+        Call<List<String>> call = restServiceClient.getCategoriesForSectorName(sectorName);
+        call.enqueue(new Callback<List<String>>() {
+            @Override
+            public void onResponse(Call<List<String>> call, Response<List<String>> response) {
+                ArrayList<String> categoriesList = (ArrayList) response.body();
+                if (categoriesList != null) {
+                    SelectFragment.setCategoryList(categoriesList);
+                } else {
+                    SelectFragment.setErrorString(ERROR_STRING);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<String>> call, Throwable t) {
+                SelectFragment.setErrorString(ERROR_STRING);
+            }
+        });
+    }
+
+    public void findSubcategoriesForCategoryName(String categoryName) {
+        RestServiceClient restServiceClient = RestServiceClient.retrofit.create(RestServiceClient.class);
+        Call<List<String>> call = restServiceClient.getSubcategoriesForCategoryName(categoryName);
+        call.enqueue(new Callback<List<String>>() {
+            @Override
+            public void onResponse(Call<List<String>> call, Response<List<String>> response) {
+                ArrayList<String> subcategoriesList = (ArrayList) response.body();
+                if (subcategoriesList != null) {
+                    SelectFragment.setSubcategoryList(subcategoriesList);
+                } else {
+                    SelectFragment.setErrorString(ERROR_STRING);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<String>> call, Throwable t) {
+                SelectFragment.setErrorString(ERROR_STRING);
+            }
+        });
+    }
+
 }
--- Frontend/PriceTag/app/src/main/java/antunmod/projects/pricetag/service/UtilService.java
+++ Frontend/PriceTag/app/src/main/java/antunmod/projects/pricetag/service/UtilService.java
@@ -1,8 +1,53 @@
 package antunmod.projects.pricetag.service;
 
-/**
- * Created by antun on 5/6/2018.
- */
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.view.View;
 
+/**
+   Util service for methods used by multiple Fragments and Activities.
+ */
 public class UtilService {
+
+
+    /**
+      Shows the progress and hides the current view.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    public void showProgress(final boolean show, final View currentView, final View loadingView) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = currentView.getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+
+            currentView.setVisibility(show ? View.GONE : View.VISIBLE);
+            currentView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    currentView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+
+            loadingView.setVisibility(show ? View.VISIBLE : View.GONE);
+            loadingView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    loadingView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+
+            currentView.setVisibility(show ? View.VISIBLE : View.GONE);
+            loadingView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+
+
+
 }
