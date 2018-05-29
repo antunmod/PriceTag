aa6b394e2d0b7dda9e8f8817e641ed7d235ff894
antunmod <antun.mod@gmail.com>
2018-05-29 20:47:42 +0200

    Added RecentProductsFragment

--- Frontend/PriceTag/app/src/main/java/antunmod/projects/pricetag/view/fragment/RecentProductsFragment.java
+++ Frontend/PriceTag/app/src/main/java/antunmod/projects/pricetag/view/fragment/RecentProductsFragment.java
@@ -6,8 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 import antunmod.projects.pricetag.R;
+import antunmod.projects.pricetag.model.GridViewAdapter;
+import antunmod.projects.pricetag.model.ImageItem;
 
 
 /**
@@ -20,12 +25,22 @@ public class RecentProductsFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private GridView gridView;
+    private GridViewAdapter gridViewAdapter;
+
+    ArrayList<ImageItem> imageItems;
+
+    View inflatedView;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recent_products, container, false);
+        inflatedView = inflater.inflate(R.layout.fragment_recent_products, container, false);
+
+        gridView = inflatedView.findViewById(R.id.gridView);
+        return inflatedView;
     }
 
 }
